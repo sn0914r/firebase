7b3c60f0b3b82afe80f5092efd2ad7fa34e87be5
sn0914r <reddysivananda83@gmail.com>
2025-04-14 11:07:55 +0530

    firestore introduction

---
+++ README.md
@@ -0,0 +1,8 @@
+# Roadmap
+1. firebase App & setUp
+2. Cloud Firestore (Database)
+3. Firebase Authentication
+4. Firebase Storage (file uploads)
+5. Firebase Cloud Functions (basic idea)
+6. Firebase Hosting
+
---
+++ firebase/setup/setup.js
@@ -0,0 +1,17 @@
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAKWCSCW1eIC81HKopqLtTrZcjwzhBKn7w",
+  authDomain: "fir-5981c.firebaseapp.com",
+  projectId: "fir-5981c",
+  storageBucket: "fir-5981c.firebasestorage.app",
+  messagingSenderId: "158025329174",
+  appId: "1:158025329174:web:5caa6f15daddbec12d2337"
+};
+
+
+const app = initializeApp(firebaseConfig);
+const fdb = getFirestore(app);
+
+export { fdb };
